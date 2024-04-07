6a6f7b8893a155b02444a67848731ca31ae13ea6
Văn Trung <137430777+vantrung1109@users.noreply.github.com>
2024-04-07 21:37:49 +0700

    mybooking_fix

--- app/src/main/java/com/base/mvvm/ui/fragment/activity/model/BookingDetail.java
+++ app/src/main/java/com/base/mvvm/ui/fragment/activity/model/BookingDetail.java
@@ -51,15 +51,17 @@ public class BookingDetail extends AbstractFlexibleItem<BookingDetail.BookingDet
         bookingDetailViewHolder.tvCreateDate.setText(createDate);
         bookingDetailViewHolder.tvPickupAddress.setText(pickupAddress);
         bookingDetailViewHolder.tvDestinationAddress.setText(destinationaAddress);
-        bookingDetailViewHolder.tvMoney.setText(String.valueOf(money));
-        if (state == 100)
-            bookingDetailViewHolder.tvState.setText("Đang chờ");
-        else if (state == 200)
-            bookingDetailViewHolder.tvState.setText("Đang chạy");
-        else if (state == 300)
-            bookingDetailViewHolder.tvState.setText("Hoàn thành");
+        bookingDetailViewHolder.tvMoney.setText(custom_money(money));
+        if (state == 300){
+            bookingDetailViewHolder.tvState.setText(bookingDetailViewHolder.itemView.getContext().getResources().getString(R.string.str_completed_booking));
+            bookingDetailViewHolder.tvState.setTextColor(bookingDetailViewHolder.itemView.getContext().getResources().getColor(R.color.completed_booking, null));
+        }
         else if (state == -100){
-            bookingDetailViewHolder.tvState.setText("Hủy");
+            bookingDetailViewHolder.tvState.setText("• Hủy");
+            bookingDetailViewHolder.tvState.setTextColor(bookingDetailViewHolder.itemView.getContext().getResources().getColor(R.color.red, null));
+        } else {
+            bookingDetailViewHolder.tvState.setText("• Đang xử lý");
+            bookingDetailViewHolder.tvState.setTextColor(bookingDetailViewHolder.itemView.getContext().getResources().getColor(R.color.processing_booking, null));
         }
 
 
@@ -80,5 +82,23 @@ public class BookingDetail extends AbstractFlexibleItem<BookingDetail.BookingDet
             tvState = view.findViewById(R.id.tv_state);
             imgVehicle = view.findViewById(R.id.img_vehicle);
         }
+    }
+
+    public String custom_money(Double money) {
+        int custom_money = money.intValue();
+        String custom_money_string = "";
+        while (custom_money / 1000 != 0){
+            int temp =  custom_money % 1000;
+            if (temp < 10){
+                custom_money_string = ".00" + String.valueOf(temp) + custom_money_string;
+            } else if (temp < 100){
+                custom_money_string = ".0" + String.valueOf(temp) + custom_money_string;
+            } else {
+                custom_money_string = "." + String.valueOf(temp) + custom_money_string;
+            }
+            custom_money = custom_money / 1000;
+        }
+        custom_money_string = String.valueOf(custom_money) + custom_money_string + " đ";
+        return custom_money_string;
     }
 }
